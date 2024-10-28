b5ff31c99f6585cbb5c49aa0e305c97c96da14cf
luisya22 <lmatosgarcia2@gmail.com>
2024-10-28 00:33:34 -0400

    feat: finished v1

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+// TODO: Check if you can run the go get
+
 // Embed the `templates` directory and its contents into the binary.
 //
 //go:embed templates/*
@@ -16,31 +18,34 @@ var templates embed.FS
 
 // Define the project file structure and the corresponding template paths in the `templates` folder.
 var fileStructure = map[string]string{
-	"/server/start.go":              "templates/start.txt",
-	"/server/server.go":             "templates/server.txt",
-	"/server/routes.go":             "templates/routes.txt",
-	"/server/helpers.go":            "templates/helpers.txt",
-	"/api/api.go":                   "templates/api.txt",
-	"/api/config.go":                "templates/config.txt",
-	"/api/context.go":               "templates/context.txt",
-	"/api/errors.go":                "templates/errors.txt",
-	"/api/helpers.go":               "templates/api_helpers.txt",
-	"/api/middleware.go":            "templates/middleware.txt",
-	"/api/server.go":                "templates/api_server.txt",
-	"/internal/assert/assert.go":    "templates/assert.txt",
-	"/internal/assert/testdata.go":  "templates/testdata.txt",
-	"/internal/assert/testutils.go": "templates/testutils.txt",
-	"/validator/validator.go":       "templates/validator.txt",
-	"/users/user_model.go":          "templates/user_model.txt",
-	"/users/user_service.go":        "templates/user_service.txt",
-	"/users/user_store.go":          "templates/user_store.txt",
-	"/cmd/{projectName}/main.go":    "templates/main.txt",
+	"/server/start.go":                 "templates/start.txt",
+	"/server/server.go":                "templates/server.txt",
+	"/server/routes.go":                "templates/routes.txt",
+	"/server/helpers.go":               "templates/helpers.txt",
+	"/api/api.go":                      "templates/api.txt",
+	"/api/config.go":                   "templates/config.txt",
+	"/api/context.go":                  "templates/context.txt",
+	"/api/errors.go":                   "templates/errors.txt",
+	"/api/helpers.go":                  "templates/api_helpers.txt",
+	"/api/middleware.go":               "templates/middleware.txt",
+	"/api/server.go":                   "templates/api_server.txt",
+	"/internal/tests/assert/assert.go": "templates/assert.txt",
+	"/internal/tests/testdata.go":      "templates/testdata.txt",
+	"/internal/tests/testutils.go":     "templates/testutils.txt",
+	"/validator/validator.go":          "templates/validator.txt",
+	"/users/user_model.go":             "templates/user_model.txt",
+	"/users/user_service.go":           "templates/user_service.txt",
+	"/users/user_store.go":             "templates/user_store.txt",
+	"/cmd/{projectName}/main.go":       "templates/main.txt",
+	"/Makefile":                        "templates/makefile.txt",
+	"/.envrc":                          "templates/env.txt",
 }
 
 func main() {
 	// Define CLI flags for the repository name and description.
 	repoName := flag.String("repo", "MyProject", "The name of the repository")
 	description := flag.String("desc", "A sample project structure created by Go CLI", "The description of the project")
+	repoPath := flag.String("repo-path", "github.com/yourusername/yourrepo", "The path/url of the repository")
 	flag.Parse()
 
 	// Replace placeholders in each file template with the provided repo name and description.
@@ -64,7 +69,7 @@ func main() {
 		// Replace placeholders with actual values.
 		updatedContent := strings.ReplaceAll(string(content), "{{RepoName}}", *repoName)
 		updatedContent = strings.ReplaceAll(updatedContent, "{{Description}}", *description)
-		updatedContent = strings.ReplaceAll(updatedContent, "{{RepoPath}}", "github.com/yourusername/"+*repoName)
+		updatedContent = strings.ReplaceAll(updatedContent, "{{RepoPath}}", *repoPath)
 
 		// Write the updated content to the target file.
 		if err := os.WriteFile(fullTargetPath, []byte(updatedContent), 0644); err != nil {
